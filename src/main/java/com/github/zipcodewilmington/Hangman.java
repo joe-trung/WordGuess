56b9eccdcc8cfca8214c7738e163be80d9be8276
joe-trung <kieutrungtran@gmail.com>
2023-03-06 21:05:20 -0500

    in progress

--- src/main/java/com/github/zipcodewilmington/Hangman.java
+++ src/main/java/com/github/zipcodewilmington/Hangman.java
@@ -1,10 +1,51 @@
 package com.github.zipcodewilmington;
 
 
+import java.lang.reflect.Array;
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * @author xt0fer
  * @version 1.0.0
  * @date 5/27/21 11:02 AM
  */
 public class Hangman {
-}
+    public static void main(String[] args) {
+
+    }
+
+    public static String[] wordList() {
+        String [] wordList = {"cowboy", "problem", "love", "console","blood", "money","school","sample","license","zipcode"};
+        return wordList;
+    }
+
+    public static String getRandomWord(String[] wordlist){
+        Random i = Random
+
+    }
+    public static int getNumberOfTries(String guessingWord) {
+        int numberOfTries = guessingWord.length();
+        return numberOfTries;
+    }
+
+    public static boolean compareGuess(char guessedChar, String guessingWord) {
+        boolean guess = false;
+        String[] temp = guessingWord.split("");
+        for (int i =0; i<temp.length;i++) {
+            if (temp[i] == guessedChar) {
+                guess = true;
+
+            }
+            System.out.println("Hoorey! your letter is in the word");
+        }
+        return guess;
+    }
+
+    public static char getGuessCharacter() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter your guessed letter:");
+    }
+
+
+} //HANG MAN OUTER CLASS
